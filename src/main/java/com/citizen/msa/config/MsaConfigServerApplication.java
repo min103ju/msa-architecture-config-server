64c9e55815b04abc4c7f2f05fd9f56940a7400b8
Min Heo <min103ju@gmail.com>
2021-04-30 05:35:34 +0900

    Msa Config Server init

---
+++ src/main/java/com/citizen/msa/config/MsaConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.citizen.msa.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class MsaConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaConfigServerApplication.class, args);
+    }
+}
